40818502eb2b87fc84268457a4c1f8cdb311dee4
MahmoudHosny <148544746+mahmoudhosnyy@users.noreply.github.com>
2023-10-22 03:16:22 +0200

    Add files via upload

---
+++ 03- HAL/02 - 7 SEGMENT/SEVENSEG_interface.h
@@ -0,0 +1,15 @@
+/****************************************/
+/* Author  : Mahmoud Hosny				*/
+/* Date    : 20 Oct 2023				*/
+/* Version : V01                        */
+/****************************************/
+
+
+#ifndef SEVENSEG_INTERFACE_H
+#define SEVENSEG_INTERFACE_H
+void SEVENSEG_init(void);
+void SEVENSEG_displayNum(u8 num);
+
+
+
+#endif
